63e8a4cf0ac44456c8570155325da66f47526d39
jiangyanwei <jiangyanwei@biaoguoworks.com>
2021-07-21 20:44:50 +0800

    revert

--- src/myStore.js
+++ src/myStore.js
@@ -2,9 +2,12 @@ const store = {
     state: {
         num: 0,
         num:1,
+<<<<<<< HEAD
         num:5,
-        num:6,
-        num:7
+        num:6
+=======
+        num:5
+>>>>>>> b4ce1bc0ca6f5d3f95d355a7a0e1eb6444f39639
     },
     add() {
         this.state.num = this.state.num + 1
